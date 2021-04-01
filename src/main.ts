cfa7a0fb868201d471d45e99aa86db249a85786e
nortondesenv <nortondesenv@users.noreply.github.com>
2021-04-01 10:58:13 -0300

    teste

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,4 @@ import { createApp } from 'vue'
 import { createHead } from '@vueuse/head'
 import App from './app/App.vue'
 
-const head = createHead()
-
-createApp(App).use(head).mount('#app')
+createApp(App).use(createHead()).mount('#app')
