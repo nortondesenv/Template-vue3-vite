414590d680af3edef5714492b088388826ad277b
nortondesenv <nortondesenv@users.noreply.github.com>
2021-04-01 09:54:16 -0300

    teste

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,14 +1,18 @@
 ﻿module.exports = {
+  parserPreset: {
+    parserOpts: {
+      headerPattern: /^(\w*)(?:\((.*)\))?:\s(.*)$/,
+      headerCorrespondence: ['type', 'scope', 'subject']
+    }
+  },
   rules: {
-    "type-case": [2, "always", "lower-case"],
-    "type-enum": [2, "always", ["chore", "ci", "docs", "feat", "fix", "test", "style"]],
-    "subject-case": [2, "always", ["sentence-case", "lower-case"]],
-    "subject-max-length": [2, "always", 50],
-    "subject-empty": [2, "never"],
-    "subject-full-stop": [2, "never", "."],
-    "body-leading-blank": [2, "always"],
-    "body-max-line-length": [2, "always", 72],
-    "footer-leading-blank": [2, "always"],
-    "footer-max-line-length": [2, "always", 72]
+    'type-empty': [2, 'never'],
+    'type-case': [2, 'always', 'lower-case'],
+    'subject-empty': [2, 'never'],
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'update', 'docs', 'style', 'refactor', 'test', 'chore', 'release', 'revert']
+    ]
   }
-};
+}
