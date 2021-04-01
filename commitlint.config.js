8358f60caf85866c78e14082b8087f71b9315426
nortondesenv <nortondesenv@users.noreply.github.com>
2021-04-01 16:54:35 -0300

    teste commit

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,19 @@
+
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  'type-enum': [2, 'always', [
+    'feat', // 功能实现
+    'fix', // 问题修复
+    'docs', // 文档相关
+    'style', // 代码风格相关
+    'refactor', // 重构
+    'test', // 编写测试
+    'revert', // 回滚代码
+    'chore',
+  ]],
+  'scope-enum': [2, 'always', [
+    'app', // 全局
+    'layout', // 布局
+    'router', // 路由
+  ]],
 }
