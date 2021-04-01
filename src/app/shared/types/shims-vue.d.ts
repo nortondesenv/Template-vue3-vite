529eacfaf00195046feb268dfffacd8b7fd45ed1
nortondesenv <nortondesenv@users.noreply.github.com>
2021-04-01 10:48:54 -0300

    teste

--- src/app/shared/types/shims-vue.d.ts
+++ src/app/shared/types/shims-vue.d.ts
@@ -1,3 +1,9 @@
+/* eslint-disable import/no-duplicates */
+
+declare interface Window {
+  // extend the window
+}
+
 declare module '*.vue' {
   import { DefineComponent } from 'vue'
   const component: DefineComponent<{}, {}, any>
